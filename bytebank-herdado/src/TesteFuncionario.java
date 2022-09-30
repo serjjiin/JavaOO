91d5fda1f919578c7f3381d86c59b73bec622312
serjin <80175077+serjjiin@users.noreply.github.com>
2022-09-30 16:55:23 -0300

    Add files via upload

---
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,16 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		
+		Funcionario nico = new Funcionario();
+		nico.setNome("Nico Steppat");
+		nico.setCpf("223344646-9");
+		nico.setSalario(2600.00);
+		
+		System.out.println(nico.getNome());
+		System.out.println(nico.getBonificacao());
+		
+	}
+
+}
